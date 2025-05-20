be62f8f8741219e4c0b1524b1ce9544602ca3792
Samantha Bianchi <samantha.bianchi@onerpm.com>
2025-05-20 19:32:55 -0300

    feat: adding ex01 from list C00

--- c/ex01/ft_print_alphabet.c
+++ c/ex01/ft_print_alphabet.c
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sambia-onerpm <sambia-onerpm@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/20 14:18:29 by sambia-oner       #+#    #+#             */
-/*   Updated: 2025/05/20 15:35:37 by sambia-oner      ###   ########.fr       */
+/*   Created: 2025/05/20 14:46:24 by sambia-oner       #+#    #+#             */
+/*   Updated: 2025/05/20 15:37:23 by sambia-oner      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_putchar (char c)
+void    ft_print_alphabet(void)
 {
-    write (1, &c, 1);
+    char    letter;
+
+    letter = 'a';
+    while (letter <= 'z')
+    {
+        write(1, &letter, 1);
+        letter++;
+    }
 }
