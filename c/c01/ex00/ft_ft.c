5357bc6404d8f2d056514ff9b4219f7a1d946ba3
Samantha Bianchi <samantha.bianchi@onerpm.com>
2025-05-28 18:06:04 -0300

    feat: adding ex01 and update ex00 from list C01

--- c/c01/ex00/ft_ft.c
+++ c/c01/ex00/ft_ft.c
@@ -6,12 +6,19 @@
 /*   By: sambia-onerpm <sambia-onerpm@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 17:26:57 by sambia-oner       #+#    #+#             */
-/*   Updated: 2025/05/28 17:40:49 by sambia-oner      ###   ########.fr       */
+/*   Updated: 2025/05/28 18:04:29 by sambia-oner      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include    <unistd.h>
+#include <stdio.h>
 
 void    ft_ft(int *nbr){
 	*nbr = 42;
+}
+
+int main (void){
+	int x = 0;
+	ft_ft(&x);
+	printf("%d\n", x);
 }
