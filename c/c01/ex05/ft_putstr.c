5fbafe415166b2bbada3068db7d65f1b4e447753
Samantha Bianchi <samantha.bianchi@onerpm.com>
2025-06-10 17:11:36 -0300

    Feat: add ex05 of list c01

---
+++ c/c01/ex05/ft_putstr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sambia-onerpm <sambia-onerpm@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 17:00:22 by sambia-oner       #+#    #+#             */
+/*   Updated: 2025/06/10 17:10:51 by sambia-oner      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	main(void)
+{
+	char	*a;
+
+	a = "samantha";
+	ft_putstr(a);
+	return (0);
+}
