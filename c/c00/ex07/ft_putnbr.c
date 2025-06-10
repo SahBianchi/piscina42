52f264b785bd5dbb2ae8671b1c530e46f91dbbf1
Samantha Bianchi <samantha.bianchi@onerpm.com>
2025-06-10 16:53:40 -0300

    Feat: add ex07 of list c00

---
+++ c/c00/ex07/ft_putnbr.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sambia-onerpm <sambia-onerpm@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 16:51:24 by sambia-oner       #+#    #+#             */
+/*   Updated: 2025/06/10 16:51:50 by sambia-oner      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+	}
+	ft_putchar(nb % 10 + '0');
+}
+
+int	main(void)
+{
+	ft_putnbr(42);
+	ft_putchar('\n');
+	ft_putnbr(-2147483648);
+	ft_putchar('\n');
+	ft_putnbr(0);
+	ft_putchar('\n');
+	return (0);
+}
