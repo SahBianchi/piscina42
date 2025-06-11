e4f2827539cdf6b7290bbfbb9a941236e756f637
Samantha Bianchi <samantha.bianchi@onerpm.com>
2025-06-11 14:09:24 -0300

    Feat: ex08 of list c02

---
+++ c/c02/ex08/ft_strlowcase.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sambia-onerpm <sambia-onerpm@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 14:06:41 by sambia-oner       #+#    #+#             */
+/*   Updated: 2025/06/11 14:08:06 by sambia-oner      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+int main()
+{
+    char texto[] = "SAMAntha";
+    printf("Antes: %s\n", texto);
+    ft_strupcase(texto);
+    printf("Depois: %s\n", texto);
+    return 0;
+}
