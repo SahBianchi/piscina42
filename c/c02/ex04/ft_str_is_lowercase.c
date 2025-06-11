7217c381679a49228ad5ce3b7b7cf4a558d5ed94
Samantha Bianchi <samantha.bianchi@onerpm.com>
2025-06-11 13:19:17 -0300

    Feat: ex04 of list c02

---
+++ c/c02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sambia-onerpm <sambia-onerpm@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 13:09:26 by sambia-oner       #+#    #+#             */
+/*   Updated: 2025/06/11 13:17:59 by sambia-oner      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+
+int	ft_str_is_lowercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] == '\0')
+		return (1);
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+// int	main(void)
+// {
+// 	printf("%d\n", ft_str_is_lowercase("ABCZ"));
+// 	printf("%d\n", ft_str_is_lowercase("abc"));
+// 	printf("%d\n", ft_str_is_lowercase(""));
+// 	printf("%d\n", ft_str_is_lowercase("00001"));
+// 	printf("%d\n", ft_str_is_lowercase("ABCdef"));
+// 	printf("%d\n", ft_str_is_lowercase("&#()"));
+// }
