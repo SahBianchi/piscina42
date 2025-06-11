ab3a31b1fc0b31c30c5f01eea749f7ea8eb4f357
Samantha Bianchi <samantha.bianchi@onerpm.com>
2025-06-11 12:21:02 -0300

    Fix: commenting on main

--- c/c02/ex01/ft_strncpy.c
+++ c/c02/ex01/ft_strncpy.c
@@ -1,16 +1,28 @@
-#include  <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sambia-onerpm <sambia-onerpm@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 12:07:45 by sambia-oner       #+#    #+#             */
+/*   Updated: 2025/06/11 12:18:41 by sambia-oner      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
-	while(src[i] != '\0' && i < n)
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while(i < n)
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
@@ -18,11 +30,12 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	return (dest);
 }
 
-int	main(void)
-{
-	char	src[] = "Samantha";
-	char	dest[3];
+// int	main(void)
+// {
+// 	char	src[] = "Samantha";
+// 	char	dest[20];
+// 	unsigned int n = 5;
 
-	ft_strncpy(dest, src, n);
-	printf("%s\n", dest);
-}
+// 	ft_strncpy(dest, src, n);
+// 	printf("%s\n", dest);
+// }
