7e38ee55245f14635ecf926223e3c90eaada6f91
Samantha Bianchi <samantha.bianchi@onerpm.com>
2025-06-16 16:13:21 -0300

    update ex00 of list c03

--- c/c03/ex00/ft_strcmp.c
+++ c/c03/ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: sambia-onerpm <sambia-onerpm@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 17:39:26 by sambia-oner       #+#    #+#             */
-/*   Updated: 2025/06/11 18:13:26 by sambia-oner      ###   ########.fr       */
+/*   Updated: 2025/06/16 16:10:53 by sambia-oner      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	ft_strcmp(char *s1, char *s2)
 // 	char	*str1;
 // 	char	*str2;
 
-// 	str1 = "samantha";
-// 	str2 = "samantha";
+// 	str1 = "ele";
+// 	str2 = "ela";
 // 	printf("%d\n", ft_strcmp(str1, str2));
 // }
